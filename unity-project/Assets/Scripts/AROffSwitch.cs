6fc50b2521b44fbb0cbe6d87ce3050c0a998b78e
John Pasquarello <john.luke.pasquarello@gmail.com>
2017-04-30 02:10:16 -0400

    started making the network calls. I worked out how the animation works in another project. it's happening

---
+++ unity-project/Assets/Scripts/AROffSwitch.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AROffSwitch : MonoBehaviour {
+  private bool en = true;
+  public Camera cam;
+  // Use this for initialization
+  void Start () {
+
+  }
+
+  // Update is called once per frame
+  void Update () {
+
+  }
+
+  public void toggle(){
+    en = !en;
+    if(en){
+      cam.clearFlags = CameraClearFlags.SolidColor;
+    }
+    else{
+      cam.clearFlags = CameraClearFlags.Skybox;
+    }
+  }
+}
---
+++ unity-project/Assets/Scripts/GUIFunctions.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GUIFunctions : MonoBehaviour {
+  public InputField loginName;
+  public InputField loginPass;
+
+  public InputField signupName;
+  public InputField signupEmail;
+  public InputField signupPass;
+
+  private string hostname = "http://bc1691.code.engineering.nyu.edu:9800/";
+
+  void Start () {}
+  void Update () {}
+
+  public void login(){
+    WWWForm postData = new WWWForm();
+    postData.AddField("username",loginName.text);
+    postData.AddField("password",loginPass.text);
+    
+    WWW www = new WWW(hostname+"loginAuth", postData);
+
+    while(!www.isDone){
+    }
+
+    print(System.Text.Encoding.UTF8.GetString(www.bytes));
+
+    //if it worked alert the user
+    //if it failed alert the user
+
+  }
+
+  public void createAcct(){
+    WWWForm postData = new WWWForm();
+    postData.AddField("username",signupName.text);
+    postData.AddField("password",signupPass.text);
+    postData.AddField("email",signupEmail.text);
+    
+    WWW www = new WWW(hostname+"registerAuth", postData);
+
+    while(!www.isDone){
+    }
+
+    print(System.Text.Encoding.UTF8.GetString(www.bytes));
+
+  }
+}
