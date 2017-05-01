62f22f14ee55dbc8beac2cdfe3dbfcf0cc3984ba
John Pasquarello <john.luke.pasquarello@gmail.com>
2017-04-30 20:37:23 -0400

    did the animations for trump, started on the food throwing

--- unity-project/Assets/Scripts/GUIFunctions.cs
+++ unity-project/Assets/Scripts/GUIFunctions.cs
@@ -19,7 +19,7 @@ public class GUIFunctions : MonoBehaviour {
     WWWForm postData = new WWWForm();
     postData.AddField("username",loginName.text);
     postData.AddField("password",loginPass.text);
-    
+
     WWW www = new WWW(hostname+"loginAuth", postData);
 
     while(!www.isDone){
@@ -28,6 +28,10 @@ public class GUIFunctions : MonoBehaviour {
     print(System.Text.Encoding.UTF8.GetString(www.bytes));
 
     //if it worked alert the user
+    if(parseResponseCode(www.responseHeaders["STATUS"]) == 500){
+      Debug.LogError("500 booo");
+
+    }
     //if it failed alert the user
 
   }
@@ -37,7 +41,7 @@ public class GUIFunctions : MonoBehaviour {
     postData.AddField("username",signupName.text);
     postData.AddField("password",signupPass.text);
     postData.AddField("email",signupEmail.text);
-    
+
     WWW www = new WWW(hostname+"registerAuth", postData);
 
     while(!www.isDone){
@@ -45,5 +49,22 @@ public class GUIFunctions : MonoBehaviour {
 
     print(System.Text.Encoding.UTF8.GetString(www.bytes));
 
+  }
+
+  //yanked this from stack overflow
+  private static int parseResponseCode(string statusLine) {
+    int ret = 0;
+
+    string[] components = statusLine.Split(' ');
+    if (components.Length < 3) {
+      Debug.LogError("invalid response status: " + statusLine);
+    }
+    else {
+      if (!int.TryParse(components[1], out ret)) {
+        Debug.LogError("invalid response code: " + components[1]);
+      }
+    }
+
+    return ret;
   }
 }
---
+++ unity-project/Assets/Scripts/shooter.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class shooter : MonoBehaviour {
+  public GameObject bulletPrefab;
+  public Transform bulletSpawn;
+  public int force;
+
+  // Use this for initialization
+  void Start () {
+  }
+
+  void Fire()
+  {
+    // Create the Bullet from the Bullet Prefab
+    var bullet = (GameObject)Instantiate(
+        bulletPrefab,
+        bulletSpawn.position,
+        bulletSpawn.rotation);
+
+    // Add velocity to the bullet
+    bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward * force;
+
+    // Destroy the bullet after 2 seconds
+    Destroy(bullet, 2.0f);        
+  }
+
+  // Update is called once per frame
+  void Update () {
+    var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
+    var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+
+    transform.Rotate(0, x, 0);
+    transform.Translate(0, 0, z);
+
+    if (Input.GetKeyDown(KeyCode.Space)){
+      Fire();
+    }
+
+
+    if (Input.GetMouseButtonDown(0)){
+      Fire();
+    }
+  }
+
+  void OnMouseDown(){
+    Fire();
+  }
+
+}
